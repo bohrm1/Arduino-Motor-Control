4ef0443ba6c57881edba57c9950dfbbfe4a176ed
bohrm1 <bohrm1@tcnj.edu>
2022-09-26 23:25:56 -0400

    Creating a test folder for stress testing the motors.

---
+++ MotorNoEncoder_steering_main/Tests/test_speed/test_speed.ino
@@ -0,0 +1,59 @@
+#include "MotorNoEncoder.h"
+#include "DriveRover.h"
+#include "RotateRover.h"
+
+#define IN1A 3
+#define IN2A 4
+#define PWMA 5
+
+#define IN1B 7
+#define IN2B 8
+#define PWMB 9
+
+#define IN1C 2
+#define IN2C 6
+#define PWMC 10
+
+#define IN1D 12
+#define IN2D 13
+#define PWMD 11
+
+MotorNoEncoder motor1;
+MotorNoEncoder motor2;
+MotorNoEncoder motor3;
+MotorNoEncoder motor4;
+
+void setup() 
+{
+  Serial.begin(9600);
+
+  pinMode(PWMA,OUTPUT);
+  pinMode(IN1A,OUTPUT);
+  pinMode(IN2A,OUTPUT);
+  
+  pinMode(PWMB,OUTPUT);
+  pinMode(IN1B,OUTPUT);
+  pinMode(IN2B,OUTPUT);
+
+  pinMode(PWMC,OUTPUT);
+  pinMode(IN1C,OUTPUT);
+  pinMode(IN2C,OUTPUT);
+  
+  pinMode(PWMD,OUTPUT);
+  pinMode(IN1D,OUTPUT);
+  pinMode(IN2D,OUTPUT);
+  
+  motor1.ConnectPins(IN1A, IN2A, PWMA);
+  motor2.ConnectPins(IN1B, IN2B, PWMB);
+  motor3.ConnectPins(IN1C, IN2C, PWMC);
+  motor4.ConnectPins(IN1D, IN2D, PWMD);
+}
+
+void loop() 
+{
+Serial.print("Turning ");
+motor1.Drive(255);
+motor2.Drive(255);
+motor3.Drive(255);
+motor4.Drive(255);
+}
