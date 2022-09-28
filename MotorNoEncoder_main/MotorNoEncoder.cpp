dd47a46da980734fe02ce5be66feabcd8214f06a
bohrm1 <bohrm1@tcnj.edu>
2022-09-27 21:46:27 -0400

    Added comments to files for motor with no encoder

--- MotorNoEncoder_main/MotorNoEncoder.cpp
+++ MotorNoEncoder_main/MotorNoEncoder.cpp
@@ -2,12 +2,19 @@
 #include "MotorNoEncoder.h"
 
 MotorNoEncoder::MotorNoEncoder(void){}
+
+//ConnectPins is used for holding the arduino pins connected to the motor instance
 void MotorNoEncoder::ConnectPins(int in1, int in2, int pwmPin) 
 {
   In1 = in1;
   In2 = in2;
   PWMPin = pwmPin;
 }
+
+//to turn motor forward, In1 HIGH and In2 LOW
+//to turn backwards, In1 LOW and In2 HIGH
+//pwr corresponds to pwm signal, value ranges from 0-255
+//use analogWrite() to send pwm signal 
 void MotorNoEncoder::Drive(int pwr) 
 {
   analogWrite(PWMPin, abs(pwr));
--- MotorNoEncoder_main/MotorNoEncoder.h
+++ MotorNoEncoder_main/MotorNoEncoder.h
@@ -5,12 +5,12 @@
 class MotorNoEncoder 
 {
   private:
-    int In1;
-    int In2;
-    int PWMPin;
+    int In1;           //arduino pin connected to In1 and In2 of motor driver for motor
+    int In2;           
+    int PWMPin;        //arduino pin connected sending pwm to be amplified by driver for motor
   public:
     MotorNoEncoder(void);
-    void ConnectPins(int in1, int in2, int pwmPin);
+    void ConnectPins(int in1, int in2, int pwmPin);     
     void Drive(int pwr);
 };
 #endif
