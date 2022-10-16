98b734f7d02a2e32d3afbcbad63744f03b65c14c
bohrm1 <bohrm1@tcnj.edu>
2022-10-16 09:25:52 -0400

    Updated pin numbers because we moved to Arduino Mega.

--- MotorNoEncoder_steering_main/MotorNoEncoder_steering_main.ino
+++ MotorNoEncoder_steering_main/MotorNoEncoder_steering_main.ino
@@ -2,21 +2,21 @@
 #include "DriveRover.h"
 #include "RotateRover.h"
 
-#define IN1A 3
-#define IN2A 4
-#define PWMA 5
+#define IN1A 24
+#define IN2A 25
+#define PWMA 4
 
-#define IN1B 7
-#define IN2B 8
-#define PWMB 9
+#define IN1B 26
+#define IN2B 27
+#define PWMB 5
 
-#define IN1C 2
-#define IN2C 6
-#define PWMC 10
+#define IN1C 28
+#define IN2C 29
+#define PWMC 6
 
-#define IN1D 12
-#define IN2D 13
-#define PWMD 11
+#define IN1D 30
+#define IN2D 31
+#define PWMD 7
 
 MotorNoEncoder motor1;
 MotorNoEncoder motor2;
@@ -53,14 +53,25 @@ void setup()
 
 void loop() 
 {
-  while(!Serial.available());
-  int drivePower = Serial.readString().toInt();
+//  while(!Serial.available());
+//  int drivePower = Serial.readString().toInt();
+  int drivePower = 255;
   int rotatePower = 30;
 
   
-  
-  DriveRover(drivePower*50, 0, motor1, motor2, motor4, motor3);
+
+//  motor1.Drive(50);
+//  
+//  motor2.Drive(0);
+//  
+//  motor3.Drive(0);
+//  
+//  motor4.Drive(0);
+
   delay(2000);
+  
+  DriveRover(drivePower, 0, motor1, motor2, motor4, motor3);
+//  delay(2000);
   
   //RotateRover(drivePower*50, motor1, motor2, motor4, motor3);
   //delay(2000);
