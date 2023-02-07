a98fb2da77ee94ae7060b9dda1c30fbfb80be75e
bohrm1 <bohrm1@tcnj.edu>
2023-02-07 14:01:45 -0500

    Fixing RotateRover to use pointers.

--- MotorNoEncoder_steering_main/RotateRover.h
+++ MotorNoEncoder_steering_main/RotateRover.h
@@ -3,11 +3,11 @@
 #include "MotorNoEncoder.h"
 #include "Arduino.h"
 
-void RotateRover (int pwr, MotorNoEncoder &motor1, MotorNoEncoder &motor2, MotorNoEncoder &motor3, MotorNoEncoder &motor4) 
+void RotateRover (int pwr, MotorNoEncoder *motor1, MotorNoEncoder *motor2, MotorNoEncoder *motor3, MotorNoEncoder *motor4) 
 {
-    motor1.Drive(pwr);
-    motor2.Drive(-pwr);
-    motor3.Drive(pwr);
-    motor4.Drive(-pwr);
+    motor1->Drive(pwr);
+    motor2->Drive(-pwr);
+    motor3->Drive(-pwr);
+    motor4->Drive(pwr);
 }
 #endif
